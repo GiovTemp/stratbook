4c566b35431a3c54c0883c584c814ffe82742a62
Peppe <piccininnig5@gmail.com>
2022-10-21 16:39:30 +0200

    Setup Map table and migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class HomeController extends Controller {
     public function Home(){
 
         return view('home');
+        
     }   
 }
---
+++ database/migrations/2022_10_21_135322_create_maps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Map;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        $maps = json_decode(Storage::disk('local')->get('json/maps.json')) -> maps;
+        foreach ($maps as $map) {
+            Map::create(['name' => $map]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maps');
+    }
+};
