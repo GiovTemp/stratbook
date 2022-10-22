f7f7b1ae7131cc697f316d7c0a09020177fa4eec
Giovanni <giovannitempestainfo@gmail.com>
2022-10-22 17:51:11 +0200

    autofill operators json

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Operator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
--- database/migrations/2022_10_21_133541_create_operators_table.php
+++ database/migrations/2022_10_21_133541_create_operators_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Operator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,14 +17,35 @@ return new class extends Migration
         Schema::create('operators', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('armor');
-            $table->integer('speed');
-            $table->string('primary');
-            $table->string('secondary');
-            $table->string('gadget');
+            $table->string('armor');
+            $table->string('speed');
+            $table->longText('primary');
+            $table->longText('secondary');
+            $table->longText('gadget');
             $table->longText('ability');
             $table->timestamps();
         });
+
+        $operators = json_decode(Storage::disk('local')->get('json/operators.json'));
+        $abilities =  json_decode(Storage::disk('local')->get('json/ability.json'));
+        $arr = [];
+
+        foreach ( $abilities as $obj ){
+            $arr[$obj->_id] = json_encode($obj);    
+        }
+
+        foreach ($operators as $operator) {
+            Operator::create([
+                'name' => $operator->name,
+                'speed' =>$operator->speed_rating,
+                'armor' =>$operator->armor_rating,
+                'primary' => json_encode($operator->primaries),
+                'secondary' => json_encode($operator->secondaries),
+                'gadget' => json_encode($operator->gadgets),
+                'ability' => json_encode($arr[$operator->ability]),
+            ]);
+        }
+
     }
 
     /**
@@ -36,3 +58,4 @@ return new class extends Migration
         Schema::dropIfExists('operators');
     }
 };
+
